38157440cec217612464eeefc04bbd133e330a4d
Nastia Gusev <gusev.nastia@gmail.com>
2022-07-11 13:06:05 +0300

    Updated WebView

---
+++ H4XOR News/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  H4XOR News
+//
+//  Created by Nastia Gusev on 10/07/2022.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
---
+++ H4XOR News/Models/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  H4XOR News
+//
+//  Created by Nastia Gusev on 11/07/2022.
+//
+
+import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView : UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
